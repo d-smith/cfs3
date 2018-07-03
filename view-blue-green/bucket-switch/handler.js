37966debb8f74f4f8157fb88c2e4ab6550d0fc5b
d-smith <doug.smith.mail@gmail.com>
2018-07-03 09:05:10 -0700

    Added route extraction

--- view-blue-green/bucket-switch/handler.js
+++ view-blue-green/bucket-switch/handler.js
@@ -1,6 +1,38 @@
 'use strict';
 
+
+const getRoutingCommand = (records) => {
+  let command = undefined;
+
+  records.forEach(r => {
+    console.log(`record: ${JSON.stringify(r)}`);
+    if(r['s3'] != undefined) {
+      let key = r['s3']['object']['key'];
+      switch(key) {
+        case "blue":
+        case "green":
+          command = key;
+          break;
+        default:
+          break;
+      }
+    }
+  });
+
+  return command;
+}
+
 module.exports.bucketHandler = (event, context, callback) => {
   console.log(`event is ${JSON.stringify(event)}`);
+  let records = event['Records'];
+  let command = getRoutingCommand(records);
+  
+  if(command == undefined) {
+    console.log('No routing command in events - exiting');
+    callback(null, { message: 'no routing command', event });
+  }
+
+  console.log(`command is set routing to ${command}`);
+
   callback(null, { message: 'everything was ok', event });
 };
