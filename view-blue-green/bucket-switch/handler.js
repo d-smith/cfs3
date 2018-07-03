fd5bc39f9e68503782887188d52288e38ab41671
d-smith <doug.smith.mail@gmail.com>
2018-07-03 13:04:48 -0700

    Added cloud formation test of current config

--- view-blue-green/bucket-switch/handler.js
+++ view-blue-green/bucket-switch/handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const cloudformation = new AWS.CloudFormation();
 
 const getRoutingCommand = (records) => {
   let command = undefined;
@@ -22,7 +24,44 @@ const getRoutingCommand = (records) => {
   return command;
 }
 
-module.exports.bucketHandler = (event, context, callback) => {
+const extractStackOutputs = (outputs) => {
+  let stackOutputs = {};
+  outputs.forEach(o => {
+    stackOutputs[o['OutputKey']] = o['OutputValue'];
+  });
+  return stackOutputs;
+}
+
+const getCurrentRouter = (stackOutputs) => {
+  if(stackOutputs['BlueLambdaArn'] == stackOutputs['ContentRouterArn']) {
+    return "blue";
+  }
+
+  if(stackOutputs['GreenLambdaArn'] == stackOutputs['ContentRouterArn']) {
+    return "green";
+  }
+
+  return undefined
+}
+
+const setRouter = async (router) => {
+  console.log(`set router: ${router}`);
+  console.log(`stack name: ${JSON.stringify(process.env.BG_STACK)}`);
+  
+  let params = {
+    StackName: process.env.BG_STACK
+  };
+
+  let response = await cloudformation.describeStacks(params).promise();
+  let stackOutputs = extractStackOutputs(response['Stacks'][0]['Outputs']);
+
+  let currentRouter = getCurrentRouter(stackOutputs);
+
+  console.log(currentRouter);
+
+}
+
+module.exports.bucketHandler = async (event, context, callback) => {
   console.log(`event is ${JSON.stringify(event)}`);
   let records = event['Records'];
   let command = getRoutingCommand(records);
@@ -33,6 +72,7 @@ module.exports.bucketHandler = (event, context, callback) => {
   }
 
   console.log(`command is set routing to ${command}`);
+  setRouter(command);
 
   callback(null, { message: 'everything was ok', event });
 };
